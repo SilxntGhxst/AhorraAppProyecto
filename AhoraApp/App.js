5214a16c3e1667e4d21bdf9657436d8f4ff90f6d
Santiago Meneses <sanmenesest17@gmail.com>
2025-11-15 16:02:04 -0600

    Limpieza y orden en el proyecto
    Elimine archivos duplicados y sin uso en el proyecto. Tambien estoy preparando la screen de iniciar sesion para empezar a realizar los cambios y requisitos del tercer parcial.

--- AhoraApp/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MenuScreen from './Screens/MenuScreen';
-
-export default function App() {
-  return <MenuScreen />;
-}
--- AhorraMasApp/screens/IniciarSesionScreen.js
+++ AhorraMasApp/screens/IniciarSesionScreen.js
@@ -1,71 +1,82 @@
-import React, { useState } from 'react';
-import { Text, StyleSheet, View, SafeAreaView, ImageBackground, TextInput, TouchableOpacity, Alert, Image} from 'react-native';
+import React, { useState } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  Image,
+} from "react-native";
 
 export default function IniciarSesionScreen() {
-  const [correo, setCorreo] = useState('');
-  const [contrasena, setContrasena] = useState('');
+  const [correo, setCorreo] = useState("");
+  const [contrasena, setContrasena] = useState("");
 
   const iniciarSesion = () => {
+    if (correo.trim() === "") {
+      Alert.alert("Error  Por favor Ingresa un Correo");
+      alert("Error  Por favor Ingresa un Correo");
 
-        if(correo.trim() === ''){
-        
-          Alert.alert("Error  Por favor Ingresa un Correo");
-          alert("Error  Por favor Ingresa un Correo");
-          
-          return;
-        }
-         if(contrasena.trim() === ''){
-        
-          Alert.alert("Error  Por favor Ingresa tu Contraseña");
-          alert("Error  Por favor Ingresa tu Contraseña");
-          
-          return;
-        }
-        const correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(!correoRegex.test(correo)){
-           alert("'Por favor, Ingresa una dirección de correo válida");
-           Alert.alert("Por favor, Ingresa una dirección de correo válida");
-           return;
+      return;
+    }
+    if (contrasena.trim() === "") {
+      Alert.alert("Error  Por favor Ingresa tu Contraseña");
+      alert("Error  Por favor Ingresa tu Contraseña");
 
-        }else{
+      return;
+    }
+    const correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!correoRegex.test(correo)) {
+      alert("'Por favor, Ingresa una dirección de correo válida");
+      Alert.alert("Por favor, Ingresa una dirección de correo válida");
+      return;
+    } else if (
+      correo.toLowerCase() === "test@app.com" &&
+      contrasena === "123456"
+    ) {
+      // SIMULACIÓN DE CREDENCIALES VÁLIDAS
+      alert("BIENVENIDO");
+      Alert.alert("BIENVENIDO");
 
-         alert("BIENVENIDO");
-           Alert.alert("BIENVENIDO");
-      
-         
-        
-          }
-           setCorreo('');
-          setContrasena('');
-      };
-  
+      // **AQUÍ VA LA NAVEGACIÓN REAL**
+      // navigation.navigate('Inicio'); // Asumiendo que 'Inicio' es el nombre de la ruta para AhorraAppScreen
+    } else if (correo.trim() !== "" && contrasena.trim() !== "") {
+      // SIMULACIÓN DE CREDENCIALES NO VÁLIDAS
+      Alert.alert("Error", "Correo o contraseña incorrectos.");
+      return;
+    }
+
+    setCorreo("");
+    setContrasena("");
+  };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-        <ImageBackground
-        source={require('../assets/fondo.png')}
+      <ImageBackground
+        source={require("../assets/fondo.png")}
         style={styles.background}
         resizeMode="repeat"
       >
         <View style={styles.card}>
           <View style={styles.iconContainer}>
-          <Image
-                source={require('../assets/piglogo.png')}
-                style={styles.icono}
-              />
+            <Image
+              source={require("../assets/piglogo.png")}
+              style={styles.icono}
+            />
           </View>
 
           <Text style={styles.titulo}>Ahorra +App</Text>
           <Text style={styles.subtitulo}>Gestiona tus finanzas personales</Text>
-           
-            <Text style={styles.subtitulo}>Aun no tienes cuenta:</Text>
+
+          <Text style={styles.subtitulo}>Aun no tienes cuenta:</Text>
 
           <View style={styles.tabContainer}>
-            
             <TouchableOpacity style={styles.tabRegis}>
               <Text style={styles.socialText}>Registrate</Text>
             </TouchableOpacity>
-            
           </View>
           <Text style={styles.subtitulo2}>Inicia Sesión</Text>
 
@@ -102,20 +113,20 @@ export default function IniciarSesionScreen() {
 
           <View style={styles.socialContainer}>
             <TouchableOpacity style={styles.socialIcon}>
-             <Image
-                source={require('../assets/flogo.png')}
+              <Image
+                source={require("../assets/flogo.png")}
                 style={styles.icono}
               />
             </TouchableOpacity>
             <TouchableOpacity style={styles.socialIcon}>
               <Image
-                source={require('../assets/glogo.png')}
+                source={require("../assets/glogo.png")}
                 style={styles.icono}
               />
             </TouchableOpacity>
             <TouchableOpacity style={styles.socialIcon}>
               <Image
-                source={require('../assets/xlogo.png')}
+                source={require("../assets/xlogo.png")}
                 style={styles.icono}
               />
             </TouchableOpacity>
@@ -129,104 +140,102 @@ export default function IniciarSesionScreen() {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#CFF6DD',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#CFF6DD",
   },
   card: {
-    backgroundColor: '#ffffff94',
-    width: '85%',
+    backgroundColor: "#ffffff94",
+    width: "85%",
     borderRadius: 20,
     padding: 20,
-    alignItems: 'center',
-    shadowColor: '#000',
+    alignItems: "center",
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 5,
   },
   iconContainer: {
-    backgroundColor: '#DFFBEA',
+    backgroundColor: "#DFFBEA",
     padding: 10,
     borderRadius: 50,
     marginBottom: 10,
   },
   titulo: {
     fontSize: 22,
-    fontWeight: 'bold',
-    color: '#21825C',
-    textAlign: 'center',
+    fontWeight: "bold",
+    color: "#21825C",
+    textAlign: "center",
   },
   subtitulo: {
-    color: '#666',
+    color: "#666",
     marginBottom: 20,
-    textAlign: 'center',
+    textAlign: "center",
   },
-    subtitulo2: {
-      fontSize:22,
-    color: '#21825C',
+  subtitulo2: {
+    fontSize: 22,
+    color: "#21825C",
     marginBottom: 20,
-    textAlign: 'center',
-    fontWeight:'bold'
+    textAlign: "center",
+    fontWeight: "bold",
   },
   tabContainer: {
-    flexDirection: 'row',
-    backgroundColor: '#e8e8e846',
+    flexDirection: "row",
+    backgroundColor: "#e8e8e846",
     borderRadius: 25,
     marginBottom: 20,
-    width: '100%',
+    width: "100%",
   },
   tabRegis: {
     flex: 1,
     borderRadius: 24,
-    alignItems: 'center',
-     backgroundColor: '#ffffff21',
-
+    alignItems: "center",
+    backgroundColor: "#ffffff21",
   },
   tabActive: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
-  
-  
+
   input: {
-    width: '100%',
+    width: "100%",
     height: 45,
-    backgroundColor: '#E8E8E8',
+    backgroundColor: "#E8E8E8",
     borderRadius: 10,
     paddingHorizontal: 15,
     marginBottom: 15,
   },
   Contra: {
-    color: '#007BFF',
+    color: "#007BFF",
     fontSize: 14,
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
     marginBottom: 15,
-    fontWeight:'500'
+    fontWeight: "500",
   },
   BotonInicio: {
-    backgroundColor: '#437C68',
+    backgroundColor: "#437C68",
     paddingVertical: 12,
     borderRadius: 10,
-    width: '100%',
+    width: "100%",
   },
   TextBoton: {
-    color: '#fff',
-    textAlign: 'center',
-    fontWeight: 'bold',
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
   },
- 
+
   socialText: {
     marginBottom: 15,
-    color: '#333',
-    fontWeight:'500'
+    color: "#333",
+    fontWeight: "500",
   },
   socialContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '60%',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "60%",
   },
   socialIcon: {
-    backgroundColor: '#ffffff3b',
+    backgroundColor: "#ffffff3b",
     borderRadius: 10,
     padding: 10,
     elevation: 3,
