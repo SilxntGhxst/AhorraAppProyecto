27cc4665a241bda4f2e182654c67dd446f91ef08
IsraelEsauRicoRamirez <124049153@upq.edu.mx>
2025-11-21 15:06:45 -0600

    Ajuste de archivo App.js para navegación

--- AhorraMasApp/App.js
+++ AhorraMasApp/App.js
@@ -1,28 +1,30 @@
-// AhorraMasApp/App.js (Modificación)
 
-import React, { useEffect, useState } from 'react';
-import MenuScreen from './screens/MenuScreen';
 
-//import { initDB } from './services/DBService'; // <-- Importacion de los servicios BD
+import React, { useEffect, useState } from 'react'; 
+
+import { NavigationContainer } from '@react-navigation/native'; 
+
+import RootNavigator from './navigation/AppNavigator'; 
+
 
 export default function App() {
   //const [dbLoaded, setDbLoaded] = useState(false);
 
   //useEffect(() => {
-    // 1. Inicializar la BD al montar el componente
-   // initDB();
-   // setDbLoaded(true); // Marcar como cargada (simulación)
-
-    // Posible pantalla de carga aquí
-  //}, []);
+    // Inicializar la BD
+  //  initDB();
+   // setDbLoaded(true); 
+ // }, []);
 
  // if (!dbLoaded) {
-    // Podemos retornar una pantalla de carga simple o null
- //   return null; 
- // }
+  //  return null; 
+  //}
 
-  // 2. Retornar MenuScreen (cambiará directamente iniciar sesion o a una pantalla de bienvenida)
+ 
   return (
-    <MenuScreen />
+    <NavigationContainer>
+   
+      <RootNavigator /> 
+    </NavigationContainer>
   );
 }
